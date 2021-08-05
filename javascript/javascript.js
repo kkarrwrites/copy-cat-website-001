07f4be8039f18e0fdc4f3972856a70327e0c6402
pizzaandchicken <57644505+pizzaandchicken@users.noreply.github.com>
2021-08-05 15:18:26 -0600

    Hamburger Menu

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,2 +1,12 @@
 // Copyright Year Update
 document.querySelector('#copyright-year').innerText = new Date().getFullYear();
+
+// Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon //
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
