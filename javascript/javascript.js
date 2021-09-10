9d59e9344b870e0fe0c13c23f315133932170f8c
pizzaandchicken <57644505+pizzaandchicken@users.noreply.github.com>
2021-09-10 14:30:58 -0600

    Top Button

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,4 +1,5 @@
 // Navigation Bar
+
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -8,7 +9,28 @@ function myFunction() {
   }
 }
 
+// Go to Top Button
+
+//Get the button
+mybutton = document.getElementById("myBtn");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
+
 // Slideshow
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -52,4 +74,5 @@ function showSlides() {
 } 
 
 // Footer - Copyright Year Update
+
 document.querySelector('#copyright-year').innerText = new Date().getFullYear();
